efdcafa137194b68a9d4f165f1c6a739f271676f
berdnaski <berdnaski1177@gmail.com>
2025-03-12 12:35:38 -0300

    fix route dinamic in deploy

--- src/app/api/checkout/[id]/route.ts
+++ src/app/api/checkout/[id]/route.ts
@@ -1,19 +1,17 @@
-import { type NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  const userId = params.id
-  
+export async function GET(request: NextRequest) {
+  const userId = request.nextUrl.pathname.split('/')[3]  
+
   if (!userId) {
     return NextResponse.json({ error: "User ID is required" }, { status: 400 })
   }
 
   try {
     const backendUrl = process.env.BACKEND_URL || "http://localhost:3333"
-
     return NextResponse.redirect(`${backendUrl}/checkout/${userId}`)
   } catch (error) {
     console.error("Error redirecting to checkout:", error)
-
     return NextResponse.json({ error: "Failed to redirect to checkout" }, { status: 500 })
   }
 }
