ce274aa8840e1e97c58a39ecbe9df6288567d7ec
Leonardo Lopes <leonardolivelopes2@gmail.com>
2025-03-28 12:11:16 -0300

    improve: add input password on login

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -22,9 +22,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           <input
             type={type}
             className={cn(
-              "border-input file:text-foreground placeholder:text-gray-300 flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              "border-gray-200 file:text-foreground placeholder:text-gray-300 flex h-11 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
               "focus-visible:border-pink-600 focus-visible:ring-pink-600/50 focus-visible:ring-2",
               "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+              !!Icon && "pl-10",
               className
             )}
             ref={ref}
